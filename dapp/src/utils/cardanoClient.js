da8d8f5c87c767b96382dc94f70039c5381e0d50
Thycrescendo <dmicholn@gmail.com>
2024-09-14 16:53:52 +0100

    utils opt

--- dapp/src/utils/cardanoClient.js
+++
@@ -1,8 +0,0 @@
-import { Loader } from 'cardano-serialization-lib-browser';
-
-// Initialize the library
-const loadCardano = async () => {
-  await Loader.load();
-};
-
-export { loadCardano, Loader };
